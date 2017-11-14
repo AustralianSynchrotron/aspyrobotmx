3a38ca3373d903b3658f2cbaa1bed740a7a67418
RobbieClarken <robbie.clarken@gmail.com>
2017-11-14 14:17:30 +1100

    Enable specifying zmq addresses

--- aspyrobotmx/cmd.py
+++ aspyrobotmx/cmd.py
@@ -10,8 +10,10 @@ from .make_safe import MakeSafe
 
 @click.command()
 @click.option('--config', type=click.Path(exists=True))
+@click.option('--update-address', default='tcp://*:2000')
+@click.option('--request-address', default='tcp://*:2001')
 @click.argument('robot-name')
-def run_server(config, robot_name):
+def run_server(config, update_address, request_address, robot_name):
     if config:
         with open(config) as file:
             config = json.load(file)
@@ -19,7 +21,9 @@ def run_server(config, robot_name):
             logging.config.dictConfig(config['logging'])
     robot = RobotMX(robot_name + ':')
     make_safe = MakeSafe()
-    server = RobotServerMX(robot, make_safe=make_safe)
+    server = RobotServerMX(robot, make_safe=make_safe,
+                           update_addr=update_address,
+                           request_addr=request_address)
     server.setup()
     while True:
         poll()
